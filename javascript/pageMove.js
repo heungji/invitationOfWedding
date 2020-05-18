34c268771441bc4ddfbd97c67a591e6b56a6527f
heungji <65164327+heungji@users.noreply.github.com>
2020-05-19 01:14:25 +0900

    Add files via upload

--- javascript/pageMove.js
+++ javascript/pageMove.js
@@ -7,5 +7,13 @@ $(function(){
    }, 500);
 
   });
+  $("footer p a").mousedown(function(){
+
+    var scrollPosition = $($(this).attr("data-target")).offset().top;
+
+   $("body").animate({scrollTop: scrollPosition
+   }, 500);
+
+  });
 
   });
