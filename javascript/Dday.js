45639f06be3c417dc3795e64a7acc683b8300b97
heungji <65164327+heungji@users.noreply.github.com>
2020-05-20 19:22:33 +0900

    Add files via upload

--- javascript/Dday.js
+++ javascript/Dday.js
@@ -1,4 +1,5 @@
 
+  $(function)(){
     var v = new Date().getFullYear(),
       z = new Date("Apr 23, " + v + " 00:00:00").getTime(),
       alert(v);
@@ -18,14 +19,14 @@
           ee = Math.floor(bb % 864e5 / 36e5),
           ff = Math.floor(bb % 36e5 / 6e4),
           gg = Math.floor(bb % 6e4 / 1e3);
-    if (y <= -1) {
-      document.getElementById("dday").innerText = `${dd}일 ${ee}시간 ${ff}분 ${gg}초`
-
-    }
-    else {
-      if (0 >= g) {
-        clearInterval(x), document.getElementById("birthday").innerHTML = "Happy Birthday!!", document.getElementById("birthday").classList.add("dday")
+      if (y <= -1) {
+        document.getElementById("dday").innerText = `${dd}일 ${ee}시간 ${ff}분 ${gg}초`
+      }else {
+        if (0 >= g) {
+          clearInterval(x), document.getElementById("birthday").innerHTML = "Happy Birthday!!", document.getElementById("birthday").classList.add("dday")
+        }else {
+          document.getElementById("dday").innerText = `${a}일 ${b}시간 ${c}분 ${d}초`
+        }
       }
-      else {
-        document.getElementById("dday").innerText = `${a}일 ${b}시간 ${c}분 ${d}초`
-      }
+    });
+  });
